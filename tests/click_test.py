d7c6e3cdd9cdfb43ac5b048757ee748017af3ddf
dhruvi1996 <ddm8@njit.edu>
2022-04-18 21:58:55 -0400

    changes in click_test.py and capital I for init_py

--- tests/click_test.py
+++ tests/click_test.py
@@ -1,3 +1,30 @@
+# import os
+#
+# from click.testing import CliRunner
+#
+# from app import create_log_folder, create_database
+#
+# runner = CliRunner()
+#
+#
+# def test_create_log_folder():
+#     response = runner.invoke(create_log_folder)
+#     assert response.exit_code == 0
+#     root = os.path.dirname(os.path.abspath(__file__))
+#     # set the name of the apps log folder to logs
+#     logdir = os.path.join(root, '../logs')
+#     response = os.path.exists(logdir)
+#     assert os.path.exists(logdir) == True
+#
+# def test_create_database():
+#     response = runner.invoke(create_database)
+#     assert response.exit_code == 0
+#     root = os.path.dirname(os.path.abspath(__file__))
+#     # set the name of the apps log folder to logs
+#     dbdir = os.path.join(root, '../database')
+#     # make a directory if it doesn't exist
+#     response = os.path.exists(dbdir)
+#     assert os.path.exists(dbdir) == True
 import os
 
 from click.testing import CliRunner
@@ -11,17 +38,14 @@ def test_create_log_folder():
     response = runner.invoke(create_log_folder)
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
-    logdir = os.path.join(root, '../logs')
+    logdir = os.path.join(root, '../app/logs')
     response = os.path.exists(logdir)
-    assert os.path.exists(logdir) == True
+    assert response == True
 
 def test_create_database():
     response = runner.invoke(create_database)
     assert response.exit_code == 0
     root = os.path.dirname(os.path.abspath(__file__))
-    # set the name of the apps log folder to logs
     dbdir = os.path.join(root, '../database')
-    # make a directory if it doesn't exist
     response = os.path.exists(dbdir)
-    assert os.path.exists(dbdir) == True
+    assert  response == True
